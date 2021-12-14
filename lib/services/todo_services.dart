38ea7fc739b321d69a7ec5ea8ee86ad3504cb608
FlutterMe <74610483+FlutterMe@users.noreply.github.com>
2021-12-14 16:35:36 +0000

    added as a folder in service file

---
+++ lib/services/todo_services.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class TodoServices {
+  Future<Response> getAllTodoRequest() async {
+    return await get(
+        Uri.parse('https://shrouded-gorge-69801.herokuapp.com/todos'));
+  }
+
+  Future<Response> createTodoRequest(
+      {required String title,
+      required String description,
+      required String dateTime}) async {
+    Map<String, String> header = {'Content-Type': 'application /json'};
+
+    Map<String, String> body = {
+      "title": title,
+      "description": description,
+      "date_time": dateTime
+    };
+    return await post(
+        Uri.parse('https://shrouded-gorge-69801.herokuapp.com/todos'),
+        body: jsonEncode(body),
+        headers: header);
+  }
+
+  Future<Response> updateTodoRequest(
+      {required bool status, required Stringid}) async {
+    Map<String, bool> body = {"status": status};
+    Map<String, String> header = {"Content-Type": "application/json"};
+
+     return await patch(Uri.parse('https://shrouded-gorge-69801.herokuapp.com/todos'),
+        body: jsonEncode(body), headers: header);
+  }
+
+  Future<Response> deleteTodoRequest(String id) async {
+    return await delete(
+        Uri.parse('https://shrouded-gorge-69801.herokuapp.com/todos/$id'));
+  }
+}
